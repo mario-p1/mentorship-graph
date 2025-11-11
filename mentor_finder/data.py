cff694c1fde0d5cebb53d388abe7cd9f8d967c21
Mario <84869552+mario-p1@users.noreply.github.com>
2025-11-11 18:32:56 +0100

    Add build_graph

--- mentor_finder/data.py
+++ mentor_finder/data.py
@@ -1,6 +1,11 @@
 from pathlib import Path
+from typing import Any
 
 import pandas as pd
+import torch
+from torch_geometric.data import HeteroData
+
+from mentor_finder.embedding import embed_text
 
 
 def load_raw_committee_csv(path: Path) -> pd.DataFrame:
@@ -18,3 +23,30 @@ def load_raw_committee_csv(path: Path) -> pd.DataFrame:
         "thesis_title_en",
     ]
     return df
+
+
+def build_graph(df: pd.DataFrame) -> tuple[HeteroData, dict[str, Any]]:
+    # Build thesis features
+    desc_embeddings = embed_text(df["thesis_desc_en"].tolist())
+    thesis_features = torch.from_numpy(desc_embeddings)
+
+    # Mentors
+    mentors = sorted(df["mentor"].unique().tolist())
+    mentors_dict = {mentor: index for index, mentor in enumerate(mentors)}
+
+    mentor_features = torch.ones((len(mentors), 1))
+
+    # Supervises relation
+    supervises_source = df["mentor"].apply(lambda mentor: mentors_dict[mentor])
+    supervises_destination = df.index.tolist()
+    supervises_features = torch.vstack(
+        [torch.IntTensor(supervises_source), torch.IntTensor(supervises_destination)]
+    )
+
+    # Build graph
+    graph = HeteroData()
+    graph["thesis"].x = thesis_features
+    graph["mentor"].x = mentor_features
+    graph["mentor", "supervises", "thesis"].edge_index = supervises_features
+
+    return graph, {"mentors_dict": mentors_dict}
--- mentor_finder/train.py
+++ mentor_finder/train.py
@@ -1,15 +1,24 @@
 from pathlib import Path
 
+
 import pandas as pd
+
+from mentor_finder.data import build_graph
 
 
 def main():
     pd.options.display.max_rows = 20
     pd.options.display.max_columns = 20
 
-    df = pd.read_csv(Path(__file__).parent.parent / "data" / "committee_train.csv")
+    df = pd.read_csv(
+        Path(__file__).parent.parent / "data" / "committee_train.csv",
+    )
+    df = df.head(5)
 
-    print(df.head())
+    data, metadata = build_graph(df)
+
+    print(data)
+    print(metadata)
 
 
 if __name__ == "__main__":
